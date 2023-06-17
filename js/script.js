242cbecce2149233fc4c83ddc09a2d2955327914
gnlda <dunlock67@gmail.com>
2023-06-17 18:28:15 +0000

    Jun 17, 2023, 9:28 PM

--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
-const slider = document.getElementById('slider');
-const sliderContent = document.getElementById('slider-content');
-let isMouseDown = false;
+const sliderContainer = document.querySelector('.slider-container');
+let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-  isMouseDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+sliderContainer.addEventListener('mousedown', (event) => {
+  isDown = true;
+  startX = event.pageX - sliderContainer.offsetLeft;
+  scrollLeft = sliderContainer.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => {
-  isMouseDown = false;
+sliderContainer.addEventListener('mouseleave', () => {
+  isDown = false;
 });
 
-slider.addEventListener('mouseup', () => {
-  isMouseDown = false;
+sliderContainer.addEventListener('mouseup', () => {
+  isDown = false;
 });
 
-slider.addEventListener('mousemove', (e) => {
-  if (!isMouseDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; // Adjust the scroll speed here
-  slider.scrollLeft = scrollLeft - walk;
-});
-
-slider.addEventListener('scroll', () => {
-  const { scrollLeft, scrollWidth, clientWidth } = slider;
-  const firstSlide = sliderContent.querySelector('.slide');
-  
-  if (scrollLeft === 0) {
-    // First slide is out of view on the left, move it to the right side
-    sliderContent.appendChild(firstSlide);
-    slider.scrollLeft = 1; // Set scrollLeft to 1 to prevent jumpy behavior
-  } else if (scrollLeft + clientWidth === scrollWidth) {
-    // Last slide is out of view on the right, move it to the left side
-    sliderContent.insertBefore(firstSlide, sliderContent.firstChild);
-    slider.scrollLeft = slider.scrollLeft - 1; // Adjust scrollLeft accordingly
-  }
+sliderContainer.addEventListener('mousemove', (event) => {
+  if (!isDown) return;
+  event.preventDefault();
+  const x = event.pageX - sliderContainer.offsetLeft;
+  const walk = (x - startX) * 2;
+  sliderContainer.scrollLeft = scrollLeft - walk;
 });
