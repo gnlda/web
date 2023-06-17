1a4777a3f6163ab20c49d2166cdb2d89dfc09066
gnlda <dunlock67@gmail.com>
2023-06-17 18:06:01 +0000

    Jun 17, 2023, 9:06 PM

--- js/script.js
+++ js/script.js
@@ -31,5 +31,5 @@ document.addEventListener("mousemove", (e) => {
         const sliderItemsArray = Array.from(sliderItems); // Преобразование NodeList в массив
         const firstItem = sliderItemsArray.shift(); // Удаление первого элемента массива и сохранение его
         sliderItemsArray.push(firstItem); // Добавление удаленного элемента в конец массива
-        slider.insertAdjacentHTML('beforeend', sl)
+        slider.insertAdjacentHTML('beforeend', sliderItemsArray[0])
 }});
