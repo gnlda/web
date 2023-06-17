517e6d83cd0cd6178725309743b4117ef89b8c34
gnlda <dunlock67@gmail.com>
2023-06-17 17:01:59 +0000

    Jun 17, 2023, 8:01 PM

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ document.addEventListener("mousemove", (e) => {
     if (startX && isUserTouching === true) {
         // console.log(slider.style.left);
         slider.style.left =  e.clientX - startX + "px";
-        let delta = +slider.style.left.substring(-2)
+        let delta = slider.style.left.substring(-2)
         console.log(delta)
     }
 });
