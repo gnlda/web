52a6969c01583c3e2ceadf2bcc8a601d40d09411
gnlda <dunlock67@gmail.com>
2023-06-17 17:44:01 +0000

    Jun 17, 2023, 8:44 PM

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ document.addEventListener("mousemove", (e) => {
     if (+slider.style.left.substring(0, slider.style.left.length - 2) < 300) {
         sliderItems = Array.from(sliderItems);
         sliderItems.push(sliderItems[0]);
+        sl
         //sliderItems[0].remove();
     }
 });
