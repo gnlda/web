892acfb6cf9cf8379c2814427bbc736c5e516392
gnlda <dunlock67@gmail.com>
2023-06-17 19:05:03 +0000

    Jun 17, 2023, 10:05 PM

--- js/script.js
+++ js/script.js
@@ -26,14 +26,14 @@ sliderContainer.addEventListener('mousemove', (event) => {
   if (!isDown) return;
   event.preventDefault();
   const x = event.pageX - sliderContainer.offsetLeft;
-  const walk = x - startX;
+  const walk = (x - startX) * 2;
   sliderContainer.scrollLeft = scrollLeft - walk;
 });
 
 sliderContainer.addEventListener('scroll', () => {
   if (sliderContainer.scrollLeft === 0) {
-    sliderContainer.scrollLeft = sliderWrapper.offsetWidth / 2;
-  } else if (sliderContainer.scrollLeft >= sliderWrapper.scrollWidth - sliderContainer.offsetWidth) {
-    sliderContainer.scrollLeft = sliderWrapper.offsetWidth / 2 - sliderContainer.offsetWidth;
+    sliderContainer.scrollLeft = sliderContainer.scrollWidth;
+  } else if (sliderContainer.scrollLeft >= sliderContainer.scrollWidth - sliderContainer.offsetWidth) {
+    sliderContainer.scrollLeft = 0;
   }
 });
