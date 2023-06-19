d6050927fce366052463091f20d0f52624de2652
revus05 <skninni05@gmail.com>
2023-06-19 17:35:37 +0000

    Jun 19, 2023, 8:35 PM

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ slider.addEventListener("mouseup", e => {
 slider.addEventListener("mousemove", e => {
     if (isTouch === true){
         //let left = slider.getPropertyValue("left");
-        console.log(window.getPropertyValue(slider, null));
+        console.log(parseInt(slider.style.left));
         slider.style.left = e.offsetX + "px";
     }
 });
