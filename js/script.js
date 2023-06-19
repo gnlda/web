6c1e93a112d64c8ce6317105e33ce469a95172b4
revus05 <skninni05@gmail.com>
2023-06-19 19:09:13 +0000

    Jun 19, 2023, 10:09 PM

--- js/script.js
+++ js/script.js
@@ -15,22 +15,24 @@ slider.style.left = -2196 + "px";
 let sliderLength = 2220;
 
 const move = (e) => {
-    if (isTouch === true) {
-        let left = parseInt(slider.style.left);
-            if (left <= -410 && left >= -2630) {
-                slider.style.left = left + e.offsetX - startLeft + "px";
-                console.log(left);
-            } else if (left < -2630) {
-                left = left + sliderLength;
-                slider.style.left = left + e.offsetX - startLeft + "px";
-                console.log(left);
-            } else if (left > -410) {
-                left = left - sliderLength;
-                slider.style.left = left + e.offsetX - startLeft + "px";
-                console.log(left);
-            }
-        }
+    
 }
 
 
-slider.addEventListener("mousemove", move(e));
+slider.addEventListener("mousemove", e => {
+    if (isTouch === true) {
+        let left = parseInt(slider.style.left);
+        if (left <= -410 && left >= -2630) {
+            slider.style.left = left + e.offsetX - startLeft + "px";
+            console.log(left);
+        } else if (left < -2630) {
+            left = left + sliderLength;
+            slider.style.left = left + e.offsetX - startLeft + "px";
+            console.log(left);
+        } else if (left > -410) {
+            left = left - sliderLength;
+            slider.style.left = left + e.offsetX - startLeft + "px";
+            console.log(left);
+        }
+    }
+});
