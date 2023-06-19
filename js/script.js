ae64749fa0827fd4d36dda884a2a5be5cf20e308
gnlda <dunlock67@gmail.com>
2023-06-19 21:59:45 +0000

    Jun 20, 2023, 12:59 AM

--- js/script.js
+++ js/script.js
@@ -42,16 +42,16 @@ slider.addEventListener("mousemove", e => {
 });
 
 
-// setInterval(() => {
-//     if (isTouch === false) {
-//         slider.style.left = parseInt(slider.style.left) - 1 + "px";
-//         let left = parseInt(slider.style.left);
-//         if (left < -sliderLength - gap) {
-//             left = left + sliderLength;
-//             slider.style.left = left - startLeft + "px";
-//         } else if (left > -gap) {
-//             left = left - sliderLength;
-//             slider.style.left = left - startLeft + "px";
-//         }
-//     }
-// }, 15);
+setInterval(() => {
+    if (isTouch === false) {
+        slider.style.left = parseInt(slider.style.left) - 1 + "px";
+        let left = parseInt(slider.style.left);
+        if (left < -sliderLength - gap) {
+            left = left + sliderLength;
+            slider.style.left = left - startLeft + "px";
+        } else if (left > -gap) {
+            left = left - sliderLength;
+            slider.style.left = left - startLeft + "px";
+        }
+    }
+}, 15);
