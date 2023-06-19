27878842f9f1d4764092a6ff60a3949a02e0d472
revus05 <skninni05@gmail.com>
2023-06-19 22:16:00 +0000

    Jun 20, 2023, 1:16 AM

--- js/script.js
+++ js/script.js
@@ -26,10 +26,10 @@ console.log(sliderLength);
 slider.addEventListener("mousemove", e => {
     if (isTouch === true) {
         let left = parseInt(slider.style.left);
-        if (left <= -offcet && left >= -sliderLength - offcet) {
+        if (left <= -offcet && left >= -sliderLength - gap) {
             slider.style.left = left + e.offsetX - startLeft + "px";
             console.log(left);
-        } else if (left < -sliderLength - offcet) {
+        } else if (left < -sliderLength - gap) {
             left = left + sliderLength;
             slider.style.left = left + e.offsetX - startLeft + "px";
             console.log(left);
