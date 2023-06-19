efcfa5acb179085bb4fda1edff4153a9808aa502
revus05 <skninni05@gmail.com>
2023-06-19 20:35:15 +0000

    Jun 19, 2023, 11:35 PM

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 let slider = document.querySelector(".slider");
+let sliderItems = document.querySelector(".slider__item");
+sliderItems.forEach(item => {
+    item.style.width = 370 + "px";
+});
 let startLeft = 0;
 let isTouch = false;
 
