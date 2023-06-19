82e0ee5889aef59285530176c5565c8530908ab2
revus05 <skninni05@gmail.com>
2023-06-19 17:24:51 +0000

    Jun 19, 2023, 8:24 PM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 let slider = document.querySelector(".slider");
 let startLeft = 0;
+let isTouch = false;
 
 slider.addEventListener("mousedown", e => {
     startLeft = e.offsetX;
+    isTouch = true;
+});
+
+slider.addEventListener("mousemove", e => {
+    slider.style.left = startLeft - e.offsetX;
 });
