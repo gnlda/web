3631a720ac111fc6d7520577b23d46e1c25897f5
revus05 <skninni05@gmail.com>
2023-06-19 19:06:28 +0000

    Jun 19, 2023, 10:06 PM

--- js/script.js
+++ js/script.js
@@ -16,24 +16,25 @@ document.addEventListener("mouseup", e => {
 slider.style.left = -2196 + "px";
 let sliderLength = 2220;
 
-slider.addEventListener("mousemove", e => {
-    if (isTouch === true) {
-    let left = parseInt(slider.style.left);
-        if (left <= -410 && left >= -2630) {
-            slider.style.left = left + e.offsetX - startLeft + "px";
-            console.log(left);
-        } else if (left < -2630) {
-            left = left + sliderLength;
-            slider.style.left = left + e.offsetX - startLeft + "px";
-            console.log(left);
-        } else if (left > -410) {
-            left = left - sliderLength;
-            slider.style.left = left + e.offsetX - startLeft + "px";
-            console.log(left);
-        }
-    }
-});
+slider.addEventListener("mousemove", move());
 
-setInterval(() => {
-    slider.style.left = parseInt(slider.style.left) - 1 + "px";
-}, 10)
+setInterval(move(), 10)
+
+
+const move = () => {
+    if (isTouch === true) {
+        let left = parseInt(slider.style.left);
+            if (left <= -410 && left >= -2630) {
+                slider.style.left = left + e.offsetX - startLeft + "px";
+                console.log(left);
+            } else if (left < -2630) {
+                left = left + sliderLength;
+                slider.style.left = left + e.offsetX - startLeft + "px";
+                console.log(left);
+            } else if (left > -410) {
+                left = left - sliderLength;
+                slider.style.left = left + e.offsetX - startLeft + "px";
+                console.log(left);
+            }
+        }
+}
