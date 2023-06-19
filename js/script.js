29fa5ad582fc37b02f921b806b4cc788df28a57e
revus05 <skninni05@gmail.com>
2023-06-19 19:23:46 +0000

    Jun 19, 2023, 10:23 PM

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ slider.addEventListener("mousemove", e => {
 
 setInterval(() => {
     if (isTouch === false) {
-        slider.style.left = parseInt(slider.style.left) - 1 + "px"
+        slider.style.left = parseInt(slider.style.left) - 0.5 + "px"
         let left = parseInt(slider.style.left);
         if (left < -2630) {
             left = left + sliderLength;
