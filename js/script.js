3bf5cfc279d0ca4a5131df01126a4c285c331c08
revus05 <skninni05@gmail.com>
2023-06-20 16:10:40 +0000

    Jun 20, 2023, 7:10 PM

--- js/script.js
+++ js/script.js
@@ -6,16 +6,11 @@ let sliderItems = document.querySelectorAll(".slider__item");
 console.log(slider.offsetWidth);
 let gap = 24;
 const sliderLength = (slider.offsetWidth + gap) / 2;
-
-
-
-
 slider.style.left = -sliderLength + gap + "px";
 let left = parseInt(slider.style.left);
 let isTouch = false;
 let offset = 20;
 let velocity = 0;
-let innerVelocity = 0;
 let previousOffset = 0;
 let velocityInterval;
 
