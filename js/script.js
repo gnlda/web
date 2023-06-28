7b9d75736da6b54723142174e9d577965c7dae78
revus05 <skninni05@gmail.com>
2023-06-28 13:42:32 +0000

    Jun 28, 2023, 4:42 PM

--- js/script.js
+++ js/script.js
@@ -132,12 +132,33 @@ document.addEventListener("touchend", documentMouseupHandler);
 let buyButton = document.querySelector("#buy");
 let sellButton = document.querySelector("#sell");
 let currentState = true; // true = buy, false = sell
+let row1 = document.querySelector("#row1");
+
+
+let row2 = document.querySelector("#row2");
+const our = `<input type="text" value="4.000">
+<div class="converter__item">
+<img src="img/USD.svg" alt="USD">
+<span>USD</span>
+</div>
+<div class="converter__arrow">
+<img src="img/Arrow.svg" alt="Arrow">
+</div>`
+const crypto = `<input type="text" value="0.000138">
+<div class="converter__item">
+<img src="img/Bitcoin.svg" alt="Bitcoin">
+<span>BTC</span>
+</div>
+<div class="converter__arrow">
+<img src="img/Arrow.svg" alt="Arrow">
+</div>`;
 
 const buyButtonClickHandler = () => {
     if (!currentState) {
         sellButton.classList.toggle("active");
         buyButton.classList.toggle("active");
-
+        row1.innerHTML = our;
+        row2.innerHTML = crypto;
         currentState = true;
     }
 }
@@ -146,7 +167,8 @@ const sellButtonClickHandler = () => {
     if (currentState) {
         sellButton.classList.toggle("active");
         buyButton.classList.toggle("active");
-
+        row1.innerHTML = crypto;
+        row2.innerHTML = our;
         currentState = false;
     }
 }
